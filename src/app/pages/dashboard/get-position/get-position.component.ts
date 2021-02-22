e78aa5dc2e505d62e5c4eb76c545d15be6439c65
Davide Bilancioni <davide.bilancioni@sferanet.net>
2021-02-22 21:59:21 +0100

    Aggiunto controllo indirizzo

--- src/app/pages/dashboard/get-position/get-position.component.ts
+++ src/app/pages/dashboard/get-position/get-position.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GeolocationService } from '@ng-web-apis/geolocation';
 import { take } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'get-position',
@@ -10,17 +11,23 @@ import { take } from 'rxjs/operators';
 export class GetPositionComponent implements OnInit {
   
   message = "";
+  postId;
 
-  constructor(private readonly geolocation$: GeolocationService) { }
+  constructor(private readonly geolocation$: GeolocationService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   getLocation() {
     this.geolocation$.pipe(take(1)).subscribe(position => this.doSomethingWithPosition(position));
+
   }
   
   doSomethingWithPosition(position: any){
     this.message = "Lat: " + position.coords.latitude + " Lon: " + position.coords.longitude;
+    this.http.post<any>('https://nominatim.openstreetmap.org/reverse?format=json&lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&zoom=18&addressdetails=1', { title: 'Angular POST Request Example' }).subscribe(data => {
+      console.log(data);
+      this.message += "--------" + data.display_name;
+  })
   }
 }
